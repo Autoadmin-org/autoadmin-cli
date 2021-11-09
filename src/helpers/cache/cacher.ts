54740421af7681fb3a56766b0c4ccbf254d3c5b3
Artuomka <r.tem@tuta.io>
2021-11-09 16:16:54 +0200

    removed unused cache and constants

--- src/helpers/cache/cacher.ts
+++ src/helpers/cache/cacher.ts
@@ -2,8 +2,6 @@ import * as LRU from 'lru-cache';
 import { Constants } from '../constants/constants';
 
 const knexCache = new LRU(Constants.DEFAULT_CONNECTION_CACHE_OPTIONS);
-const tunnelCache = new LRU(Constants.DEFAULT_TUNNEL_CACHE_OPTIONS);
-const driverCache = new LRU(Constants.DEFAULT_DRIVER_CACHE_OPTIONS);
 
 export class Cacher {
   public static getCachedKnex(connectionConfig): any {
@@ -13,32 +11,6 @@ export class Cacher {
 
   public static setKnexCache(connectionConfig, newKnex): void {
     knexCache.set(JSON.stringify(connectionConfig), newKnex);
-  }
-
-  public static getTunnelCache(connection): any {
-    const cachedTnl = tunnelCache.get(JSON.stringify(connection));
-    return cachedTnl ? cachedTnl : null;
-  }
-
-  public static setTunnelCache(connection, tnlObj): void {
-    tunnelCache.set(JSON.stringify(connection), tnlObj);
-  }
-
-  public static delTunnelCache(connection, tnlObj): void {
-    tunnelCache.del(JSON.stringify(connection), tnlObj);
-  }
-
-  public static getDriverCache(connection): any {
-    const cachedDriver = driverCache.get(JSON.stringify(connection));
-    return cachedDriver ? cachedDriver : null;
-  }
-
-  public static setDriverCache(connection, newDriver): void {
-    driverCache.set(JSON.stringify(connection), newDriver);
-  }
-
-  public static delDriverCache(connection): void {
-    driverCache.del(JSON.stringify(connection));
   }
 
   public static async clearKnexCache() {
